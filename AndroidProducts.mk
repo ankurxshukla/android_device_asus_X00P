d9fc0cdbd1fef85879a99c5e16f2bb8fdb5f6d75
Dan Willemsen <dwillemsen@google.com>
2019-09-15 12:27:43 +0200

    X00P: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_X00P.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_X00P-user \
+    lineage_X00P-userdebug \
+    lineage_X00P-eng
